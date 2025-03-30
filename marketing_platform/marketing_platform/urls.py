f72e61536db04f7468e941730656529f3a61a540
Shreyash Ingle <141054759+shreyash0019@users.noreply.github.com>
2025-03-30 15:22:33 +0530

    Delete marketing_platform directory

--- marketing_platform/marketing_platform/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # Replace 'your_app_name' with your actual app name
-]
